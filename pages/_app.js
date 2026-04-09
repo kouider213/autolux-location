8d21ea68e7498a2efe9d98de868d6d207b41ae28
kouider213 <doubakouider@gmail.com>
2026-04-09 05:58:34 +0200

    Rename AutoLux to Fik Conciergerie in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
               <link rel="apple-touch-icon" href="/icons/icon-192.png" />
               <meta name="apple-mobile-web-app-capable" content="yes" />
               <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-              <title>AutoLux Location</title>
+              <title>Fik Conciergerie</title>
       </Head>
         <Component {...pageProps} />
         <Toaster
